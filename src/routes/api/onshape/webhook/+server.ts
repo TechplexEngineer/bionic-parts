360aecf5addc8c712408e0fdee4717d74186c667
Blake Bourque <techplex.engineer@gmail.com>
2023-05-30 11:30:50 -0400

    Properly download step

--- src/routes/api/onshape/webhook/+server.ts
+++ src/routes/api/onshape/webhook/+server.ts
@@ -31,31 +31,53 @@ export const POST = (async (event) => {
 
         if (translation.requestState != BTTranslationRequestState.Done) {
             console.log(`translation ${translationId} status ${translation.requestState}, unable to proceed`)
-            return new Response();
+            if (translation.requestState == BTTranslationRequestState.Failed) {
+                return new Response(); // @todo send error to remove webhook
+            } else {
+                return new Response(); // webhook should be called again later
+            }
         }
 
         console.log("translation", translation) // @todo not sure which fields are populated
+        // {
+        //     documentId: 'da2bc7f409791a8720b27217',
+        //     href: 'https://cad.onshape.com/api/v6/translations/64760fe6c1e696296a252e59',
+        //     id: '64760fe6c1e696296a252e59',
+        //     name: 'Part Studio 1 - Part 2',
+        //     requestElementId: 'dfc0766722250803423263f8',
+        //     requestState: 'DONE',
+        //     resultDocumentId: 'da2bc7f409791a8720b27217',
+        //     resultExternalDataIds: [ '64760fec47c6ac151b4fb9db' ],
+        //     versionId: 'e6dfc5a88fdafa4560bfa609'
+        // }
 
         // translation.requestElementId; //which are these are used when?
-        translation.resultElementIds
+        if (!translation.resultExternalDataIds || !translation.resultDocumentId) {
+            return new Response(); //@todo send error to remove webhook
+        }
 
-        for (const resultElementId of translation.resultElementIds!) {
-            const file = await Onshape.BlobElementApi.downloadFileWorkspace({
-                did: translation.resultDocumentId!,
-                wid: translation.resultWorkspaceId!,
-                eid: resultElementId,
-                // contentDisposition: "Test"
-            }, {
-                headers: {
-                    Accept: 'application/octet-stream'
-                }
+        for (const resultElementId of translation.resultExternalDataIds) {
+            const file = await Onshape.DocumentApi.downloadExternalData({
+                did: translation.resultDocumentId,
+                fid: resultElementId,
             })
+
+            // const file = await Onshape.BlobElementApi.downloadFileWorkspace({
+            //     did: translation.resultDocumentId,
+            //     wid: translation.resultWorkspaceId!,
+            //     eid: resultElementId,
+            //     // contentDisposition: "Test"
+            // }, {
+            //     headers: {
+            //         Accept: 'application/octet-stream'
+            //     }
+            // })
             await trelloClient.cards.createCardAttachment({
                 id: event.cardId,
                 file: file,
-                name: ""
+                name: `${translation.name}.step`
             })
-            // @todo delete webhook
+            // @todo delete webhook, could return an error which would remove the webhook
         }
     } catch (e) {
         console.log("Error processing webhook", e, body)
