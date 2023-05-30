4b84df721f042017147a21133bee220207f12314
Blake Bourque <techplex.engineer@gmail.com>
2023-05-30 09:45:58 -0400

    Had backwards logic for webhook event type

--- src/routes/api/onshape/webhook/+server.ts
+++ src/routes/api/onshape/webhook/+server.ts
@@ -18,49 +18,49 @@ export const POST = (async (event) => {
     }
 
     if (body.event != "onshape.model.translation.complete") {
-        const translationId = body.translationId!
-        try {
-            const event = JSON.parse(body.data) as unknown as WebhookUserData;
-
-            const Onshape = await getOnshapeClient(event.tokenInfo);
-
-            const translation = await Onshape.TranslationApi.getTranslation({tid: translationId});
-
-            if (translation.requestState != BTTranslationRequestState.Done) {
-                console.log(`translation ${translationId} status ${translation.requestState}, unable to proceed`)
-                return new Response();
-            }
-
-            console.log("translation", translation) // @todo not sure which fields are populated
-
-            // translation.requestElementId; //which are these are used when?
-            translation.resultElementIds
-
-            for (const resultElementId of translation.resultElementIds!) {
-                const file = await Onshape.BlobElementApi.downloadFileWorkspace({
-                    did: translation.resultDocumentId!,
-                    wid: translation.resultWorkspaceId!,
-                    eid: resultElementId,
-                    // contentDisposition: "Test"
-                }, {
-                    headers: {
-                        Accept: 'application/octet-stream'
-                    }
-                })
-                await trelloClient.cards.createCardAttachment({
-                    id: event.cardId,
-                    file: file,
-                    name: ""
-                })
-                // @todo delete webhook
-            }
-        } catch (e) {
-            console.log("Expected event data to be json. got:", body.data)
-        }
-    } else {
         console.log("unhandled event", body.event, body)
+        return new Response();
     }
+    const translationId = body.translationId!
+    try {
+        const event = JSON.parse(body.data) as unknown as WebhookUserData;
 
+        const Onshape = await getOnshapeClient(event.tokenInfo);
 
+        const translation = await Onshape.TranslationApi.getTranslation({tid: translationId});
+
+        if (translation.requestState != BTTranslationRequestState.Done) {
+            console.log(`translation ${translationId} status ${translation.requestState}, unable to proceed`)
+            return new Response();
+        }
+
+        console.log("translation", translation) // @todo not sure which fields are populated
+
+        // translation.requestElementId; //which are these are used when?
+        translation.resultElementIds
+
+        for (const resultElementId of translation.resultElementIds!) {
+            const file = await Onshape.BlobElementApi.downloadFileWorkspace({
+                did: translation.resultDocumentId!,
+                wid: translation.resultWorkspaceId!,
+                eid: resultElementId,
+                // contentDisposition: "Test"
+            }, {
+                headers: {
+                    Accept: 'application/octet-stream'
+                }
+            })
+            await trelloClient.cards.createCardAttachment({
+                id: event.cardId,
+                file: file,
+                name: ""
+            })
+            // @todo delete webhook
+        }
+    } catch (e) {
+        console.log("Expected event data to be json. got:", body.data)
+    }
     return new Response();
+
+
 }) satisfies RequestHandler;
