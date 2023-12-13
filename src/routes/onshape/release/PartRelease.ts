2c33bd846659f6171938afbad6b3bc4e4d817f55
Blake Bourque <techplex.engineer@gmail.com>
2023-12-13 17:05:48 -0500

    Add quantity to stl name

--- src/routes/onshape/release/PartRelease.ts
+++ src/routes/onshape/release/PartRelease.ts
@@ -258,7 +258,7 @@ ${data.cotsLink ? `COTS Link: ${data.cotsLink}` : ""}`,
         await trelloClient.cards.createCardAttachment({
             id: card.id,
             file: await res.blob(),
-            name: `${cardName}.stl`
+            name: `${cardName} QTY:${data.qty}.stl`
         })
 
         if (data.printerUsed == Printers.FormLabs) {
