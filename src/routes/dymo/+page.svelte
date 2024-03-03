aa531fbc394b8ff8697fecc4730e174d08db8baa
Blake Bourque <techplex.engineer@gmail.com>
2024-03-03 14:01:46 -0500

    3 no rotate

--- src/routes/dymo/+page.svelte
+++ src/routes/dymo/+page.svelte
@@ -31,18 +31,18 @@
 
 			// // save the unrotated context of the canvas so we can restore it later
 			// // the alternative is to untranslate & unrotate after drawing
-			ctx.save();
+			// ctx.save();
 
 			// // move to the center of the canvas
-			ctx.translate(canvas.width/2,canvas.height/2);
+			// ctx.translate(canvas.width/2,canvas.height/2);
 
 			// rotate the canvas to the specified degrees
-			ctx.rotate(90*Math.PI/180);
+			// ctx.rotate(90*Math.PI/180);
 
 			// draw the image
 			// since the context is rotated, the image will be rotated also
 
-			ctx.drawImage(image, -image.width/2, -image.height/2);
+			ctx.drawImage(image, 0, 0);
 
 			// // we’re done with the rotating so restore the unrotated context
 			// ctx.restore();
