56b9e0306dd007e5755ffc8b0efb18ab663a4cfc
Blake Bourque <techplex.engineer@gmail.com>
2025-02-05 19:57:55 -0500

    Printable KanbanCard generator

--- src/routes/kanban/+page.svelte
+++ src/routes/kanban/+page.svelte
@@ -1,11 +1,159 @@
 <script lang="ts">
+    import { page } from '$app/stores';
+	import { links } from '../l/links';
 	import QrCode from '../qrcode/QRCode.svelte';
-	import type { PageData } from './$types';
+	import {buildShortLinkUrl} from '../l/links';
+	import KanbanCard from './KanbanCard.svelte';
 
-	export let data: PageData;
+    // interface Item {
+    //     name: string;
+    //     partNumber?: string;
+	// 	vendor?: string;
+    //     quantity: number;
+	// 	location?: string;
+    // }
 
-	let cards = [
+
+
+    // const items: Item[] = [
+    //     {
+	// 		name: `#9 Drill Bits [12 pk]`,
+	// 		partNumber: "DWDCO9P12",
+	// 		vendor: 'amz',
+	// 		quantity: 1,
+	// 		location: "Husky Tool Box"
+	// 	},
+	// 	{
+	// 		name: `#9 Drill Bit Extended Length`,
+	// 		partNumber: '3096A319',
+	// 		vendor: 'mcm',
+	// 		quantity: 2,
+	// 		location: "Husky Tool Box"
+	// 	},
+
+	// 	{
+	// 		name: `#7 Drill Bits [12 pk]`,
+	// 		partNumber: 'KFD7P12',
+	// 		vendor: 'amz',
+	// 		quantity: 1,
+	// 		location: "Husky Tool Box"
+	// 	},
+	// 	{
+	// 		name: `#7 Drill Bit Extended Length`,
+	// 		partNumber: '3096A317',
+	// 		quantity: 2,
+	// 		location: "Husky Tool Box"
+	// 	},
+
+	// 	{
+	// 		name: `#21 Drill Bits [12 pk]`,
+	// 		partNumber: 'KFD21P12',
+	// 		vendor: 'amz',
+	// 		quantity: 1,
+	// 		location: "Husky Tool Box"
+	// 	},
+	// 	{
+	// 		name: `#21 Drill Bit Extended Length`,
+	// 		partNumber: '3096A333',
+	// 		vendor: 'mcm',
+	// 		quantity: 2,
+	// 		location: "Husky Tool Box"
+	// 	},
+
+	// 	{
+	// 		name: `F Drill Bits [12 pk]`,
+	// 		partNumber: 'KFDFP12',
+	// 		vendor: 'amz',
+	// 		quantity: 1,
+	// 		location: "Husky Tool Box"
+	// 	},
+	// 	{
+	// 		name: `F Drill Bit Extended Length`,
+	// 		vendor: 'mcm',
+	// 		partNumber: '3110A56',
+	// 		quantity: 2,
+	// 		location: "Husky Tool Box"
+	// 	}
+        
+    // ];
+
+    let cards = [
 		{
+			name: `4" Zip Ties`,
+			desc: `Kable Kontrol™ Black Zip Ties - 4" Inch Long - UV Resistant Nylon - 18 Lbs Tensile Strength - 1000 pc Pack`,
+			pn: `ct208`,
+			minQty: 1,
+			reorderQty: 4,
+			location: 'W8-C',
+			department: 'Electrical',
+			img: 'https://www.cabletiesandmore.com/images/gallery/ct208-kable-kontrol-4-inch-18-lbs-black-cable-ties.jpg',
+			link: 'https://www.cabletiesandmore.com/black-zip-ties-uv?pid=15',
+			rev: '1',
+			revDate: '2 Feb 25'
+		},
+		{
+			name: `8" Zip Ties`,
+			desc: `Kable Kontrol™ Black Zip Ties - 8" Inch Long - UV Resistant Nylon - 50 Lbs Tensile Strength - 1000 pc Pack`,
+			pn: `ct209`,
+			minQty: 1,
+			reorderQty: 4,
+			location: 'W8-B',
+			department: 'Electrical',
+			img: 'https://www.cabletiesandmore.com/images/gallery/kable-kontrol-black-cable-zip-ties-ct209-1.jpg',
+			link: 'https://www.cabletiesandmore.com/black-zip-ties-uv?pid=54',
+			rev: '1',
+			revDate: '2 Feb 25'
+		},{
+			name: `4" Zip Ties`,
+			desc: `Kable Kontrol™ Black Zip Ties - 4" Inch Long - UV Resistant Nylon - 18 Lbs Tensile Strength - 1000 pc Pack`,
+			pn: `ct208`,
+			minQty: 1,
+			reorderQty: 4,
+			location: 'W8-C',
+			department: 'Electrical',
+			img: 'https://www.cabletiesandmore.com/images/gallery/ct208-kable-kontrol-4-inch-18-lbs-black-cable-ties.jpg',
+			link: 'https://www.cabletiesandmore.com/black-zip-ties-uv?pid=15',
+			rev: '1',
+			revDate: '2 Feb 25'
+		},
+		{
+			name: `8" Zip Ties`,
+			desc: `Kable Kontrol™ Black Zip Ties - 8" Inch Long - UV Resistant Nylon - 50 Lbs Tensile Strength - 1000 pc Pack`,
+			pn: `ct209`,
+			minQty: 1,
+			reorderQty: 4,
+			location: 'W8-B',
+			department: 'Electrical',
+			img: 'https://www.cabletiesandmore.com/images/gallery/kable-kontrol-black-cable-zip-ties-ct209-1.jpg',
+			link: 'https://www.cabletiesandmore.com/black-zip-ties-uv?pid=54',
+			rev: '1',
+			revDate: '2 Feb 25'
+		},{
+			name: `4" Zip Ties`,
+			desc: `Kable Kontrol™ Black Zip Ties - 4" Inch Long - UV Resistant Nylon - 18 Lbs Tensile Strength - 1000 pc Pack`,
+			pn: `ct208`,
+			minQty: 1,
+			reorderQty: 4,
+			location: 'W8-C',
+			department: 'Electrical',
+			img: 'https://www.cabletiesandmore.com/images/gallery/ct208-kable-kontrol-4-inch-18-lbs-black-cable-ties.jpg',
+			link: 'https://www.cabletiesandmore.com/black-zip-ties-uv?pid=15',
+			rev: '1',
+			revDate: '2 Feb 25'
+		},
+		{
+			name: `8" Zip Ties`,
+			desc: `Kable Kontrol™ Black Zip Ties - 8" Inch Long - UV Resistant Nylon - 50 Lbs Tensile Strength - 1000 pc Pack`,
+			pn: `ct209`,
+			minQty: 1,
+			reorderQty: 4,
+			location: 'W8-B',
+			department: 'Electrical',
+			img: 'https://www.cabletiesandmore.com/images/gallery/kable-kontrol-black-cable-zip-ties-ct209-1.jpg',
+			link: 'https://www.cabletiesandmore.com/black-zip-ties-uv?pid=54',
+			rev: '1',
+			revDate: '2 Feb 25'
+		},{
 			name: `4" Zip Ties`,
 			desc: `Kable Kontrol™ Black Zip Ties - 4" Inch Long - UV Resistant Nylon - 18 Lbs Tensile Strength - 1000 pc Pack`,
 			pn: `ct208`,
@@ -32,76 +180,113 @@
 			revDate: '2 Feb 25'
 		}
 	];
+
+    // const buildOrderUrl = (item: Item, currentUrl: URL) => {
+        
+    //     const address = new URL(currentUrl.origin + "/order");
+    //     address.searchParams.append("i", item.name);
+    //     item.partNumber && address.searchParams.append("pn", item.partNumber);
+	// 	item.vendor && address.searchParams.append("v", item.vendor);
+    //     address.searchParams.append("q", item.quantity.toString());
+    //     return address.toString();
+    // }
+
+	// const labels: {title: string, subtitle?: string, qrCodeValue?: string}[] = [
+	// 	// {
+	// 	// 	title: "",
+	// 	// 	subtitle: "",
+	// 	// 	qrCodeValue: ""
+	// 	// }
+	// ];
+	// for (const item of items) {
+	// 	labels.push({
+	// 		title: item.name,
+	// 		subtitle: `Add to Order Sheet`,
+	// 		qrCodeValue: buildOrderUrl(item, $page.url)
+	// 	});
+	// 	if (item.location) {
+	// 		labels.push({
+	// 			title: item.name,
+	// 			subtitle: `Restock From ${item.location}`,
+	// 			qrCodeValue: undefined //buildOrderUrl(item, $page.url)
+	// 		});
+	// 	}
+	// }
+
+	// for (const [key, {title, link, desc}] of Object.entries(links)) {
+	// 	labels.push({
+	// 		title: title || "",
+	// 		subtitle: desc,
+	// 		qrCodeValue: buildShortLinkUrl(key, $page.url)
+	// 	});
+		
+	// }
+
+    const numPerPage = 2;
 </script>
 
-<div class="d-flex printBg">
-	{#each cards as card}
-		<div
-			class="card1 bg-white p-2 rounded"
-			style="width: 4in;height: 6in;border: 1px solid #ccc; margin-left: 5px"
-		>
-			<div class="card-body1">
-				<div class="float-end">
-					<QrCode value={card.link} className="qrcode" width={100} />
-				</div>
+{#each [...Array(Math.ceil(cards.length / numPerPage)).keys()] as pageIdx}
+	<div class="page">
+		{#each cards.slice(pageIdx * numPerPage, (pageIdx + 1) * numPerPage) as card}
+			<div class="label">
+                <KanbanCard card={card} />
 
-				<h5 class="card-title fw-bolder">{card.name}</h5>
-				<h6 class="card-subtitle text-body-secondary mb-3">{card.pn}</h6>
-
-				<p
-					class="card-text text-body-secondary mb-1"
-					style="font-size: .8rem; height:.6in; overflow: hidden; text-overflow: ellipsis;white-space: wrap;"
-				>
-					{card.desc}
-				</p>
-
-				<div class="row" style="margin-left: 0rem; margin-right: 0rem;">
-					<div class="p-1 col-6 text-center">
-						<div class="rounded bg-danger-subtle">
-							Min QTY
-							<div class="fw-bold text-danger fs-3">{card.minQty}</div>
-						</div>
-					</div>
-					<div class="p-1 col-6 text-center">
-						<div class="rounded bg-primary-subtle">
-							Reorder QTY
-							<div class="fw-bold text-primary fs-3">{card.reorderQty}</div>
-						</div>
-					</div>
-					<div class="p-1 col-12">
-						<div class="p-2 rounded bg-secondary-subtle">
-							<div class="fw-bold">Location</div>
-							<div>{card.location}</div>
-						</div>
-					</div>
-					<div class="p-1 col-12">
-						<div class="p-2 rounded bg-warning-subtle">
-							<div class="fw-bold">Department</div>
-							<div>{card.department}</div>
-						</div>
-					</div>
-					<div class="p-1 col-12 text-center">
-						<div class="border rounded">
-							<img src={card.img} class="img-fluid" style="height: 2in; padding: 5px" alt="" />
-						</div>
-					</div>
-					<div class="ps-3 row text-small text-center" style="font-size: .8rem; color: #a0a0a0">
-						Revision: {card.rev} &mdash; {card.revDate}
-					</div>
-				</div>
-
-				<!-- <div class="row text-center gap-1" style="height: .75in">
-				<div class="col-6 border rounded" />
-				<div class="col-6 border rounded" style="height: .75in" />
-			</div> -->
 			</div>
-		</div>
-	{/each}
-</div>
+		{/each}
+		
+	</div>
+{/each}
 
-<style>
-	.printBg {
-		-webkit-print-color-adjust: exact !important;
-		print-color-adjust: exact !important;
+<style lang="scss">
+
+	.page {
+		// display: grid;
+		// grid-template-columns: 4in 4in;
+		// grid-column-gap: 0.14in;
+
+		width: 8.5in; // paper width
+		padding-top: 0.5in; //Top Margin
+		padding-bottom: 0.5in; //Top Margin
+		padding-left: 0.21975in; //Side Margin
+		outline: 1px dotted;
+
+        break-inside: avoid;
 	}
+
+    @media print {
+        .page {
+            outline: none;
+        }
+    }
+
+	.label {
+        display: inline-block;
+		box-sizing: border-box;
+		
+		// width: 4in; // Label Width
+		// height: 6in; //Label Height
+
+		outline: 1px dotted; //outline doesn't occupy space like border does
+        border-radius: 5px;
+        margin-right: 10px;
+	}
+
+    // .page:first-child .label {
+    //     margin-left: -5px;
+    // }
+
+	.vert-center {
+		display: inline-block;
+		vertical-align: middle;
+		// line-height: normal;
+	}
+
+    // .qrcode {
+	// 	width: .6in;
+    //     min-width: .6in;
+        
+	// 	// float: right;
+	// }
+
+    
 </style>
