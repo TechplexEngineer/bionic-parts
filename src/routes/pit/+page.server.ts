9e29ccb342283be7835f577dc7dd686285218b75
Blake Bourque <techplex.engineer@gmail.com>
2025-03-08 22:09:38 -0500

    Fill in most of the placeholders

--- src/routes/pit/+page.server.ts
+++ src/routes/pit/+page.server.ts
@@ -19,8 +19,11 @@ export const load = (async () => {
     const matches = await matchesRequest.json<StatboticsTeamMatches>();
 
 
-    const rankingsRequest = await fetch(`https://api.statbotics.io/v3/team_events?year=${year}&event=${eventKey}&metric=rank&ascending=true&limit=5`);
+    const rankingsRequest = await fetch(`https://api.statbotics.io/v3/team_events?year=${year}&event=${eventKey}&metric=rank&ascending=true&limit=4`);
     const rankings = await rankingsRequest.json<StatboticsTeamEvent>();
+
+    const ourRankingsRequest = await fetch(`https://api.statbotics.io/v3/team_events?team=${team}&year=${year}&event=${eventKey}&metric=rank&ascending=true&limit=5`);
+    const ourRanking = await ourRankingsRequest.json<StatboticsTeamEvent>();
 
     return {
         eventKey,
@@ -28,7 +31,42 @@ export const load = (async () => {
         year,
         event,
         teamYear,
-        matches,
-        rankings
+        upcommingMatches: matches.filter((m) => m.result.winner == null)
+            .map((m) => {
+                let color = 'Unknown';
+                if (
+                    m.alliances.red.team_keys.includes(team) ||
+                    m.alliances.red.surrogate_team_keys.includes(team)
+                ) {
+                    color = 'Red';
+                } else if (
+                    m.alliances.blue.team_keys.includes(team) ||
+                    m.alliances.blue.surrogate_team_keys.includes(team)
+                ) {
+                    color = 'Blue';
+                }
+
+                const now = new Date();
+                const duration = Math.round((new Date(m.time * 1000).getTime() - now.getTime()) / 60000);
+                const hours = Math.floor(duration / 60);
+                const minutes = duration % 60;
+                const seconds = Math.floor((new Date(m.time * 1000).getTime() - now.getTime()) / 1000) % 60;
+                const formattedDuration = `~${hours}h ${minutes}m ${seconds}s`;
+
+                return {
+                    match: m.key.replace(`${eventKey}_`, '').toUpperCase(),
+                    predictedTime: formattedDuration,
+                    color: color
+                };
+            }),
+        nextmatch: matches.filter((m) => m.result.winner == null)[0],
+        rankings: rankings.map((t) => {
+            return {
+                rank: t.record.qual.rank,
+                team: `${t.team} - ${t.team_name}`,
+                epa: JSON.stringify(t.epa.total_points.mean, null, 2)
+            };
+        }),
+        ourRanking: ourRanking[0]
     };
 }) satisfies PageServerLoad;
--- src/routes/pit/+page.svelte
+++ src/routes/pit/+page.svelte
@@ -7,44 +7,12 @@
 
 	export let data: PageData;
 
+	const titleCase = (str: string) => {
+		return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+	};
+
 	// let { data }: { data: PageData } = $props();
-	let rankData: { rank: number; team: string; epa: string }[] = data.rankings.map((t) => {
-		return {
-			rank: t.record.qual.rank,
-			team: `${t.team} - ${t.team_name}`,
-			epa: JSON.stringify(t.epa.total_points.mean, null, 2)
-		};
-	});
 
-	let upcommingMatches = data.matches
-		.filter((m) => m.result.winner == null)
-		.map((m) => {
-			let color = 'Unknown';
-			if (
-				m.alliances.red.team_keys.includes(data.team) ||
-				m.alliances.red.surrogate_team_keys.includes(data.team)
-			) {
-				color = 'Red';
-			} else if (
-				m.alliances.blue.team_keys.includes(data.team) ||
-				m.alliances.blue.surrogate_team_keys.includes(data.team)
-			) {
-				color = 'Blue';
-			}
-
-			const now = new Date();
-			const duration = Math.round((new Date(m.time * 1000).getTime() - now.getTime()) / 60000);
-			const hours = Math.floor(duration / 60);
-			const minutes = duration % 60;
-			const seconds = Math.floor((new Date(m.time * 1000).getTime() - now.getTime()) / 1000) % 60;
-			const formattedDuration = `~${hours}h ${minutes}m ${seconds}s`;
-
-			return {
-				match: m.key.replace(`${data.eventKey}_`, '').toUpperCase(),
-				predictedTime: formattedDuration,
-				color: color
-			};
-		});
 	const matchesColumns: TableColumns = [
 		{ data: 'match', title: 'Match' },
 		{ data: 'predictedTime', title: 'Predicted Time' },
@@ -53,61 +21,92 @@
 </script>
 
 <div class="container-fluid mt-3">
+	<!-- Logo Row -->
 	<div class="row">
 		<div class="col">
 			<img src="bionics-logo.svg" alt="Bionics Logo" class="logo" />
 		</div>
 	</div>
+
+	<!-- Rankings and Matches -->
 	<div class="row">
 		<div class="col">
 			<div class="bin">
 				<h4>Rankings</h4>
-				<TableForObjectArray data={rankData.sort((a, b) => a.rank - b.rank)} />
+				<TableForObjectArray data={data.rankings.sort((a, b) => a.rank - b.rank)} />
 			</div>
 		</div>
 		<div class="col">
 			<div class="bin">
-				<h4>Next Match: qm??</h4>
+				<h4>Next Match: {data.nextmatch.match_name}</h4>
 
 				<div class="row mb-3">
 					<div class="col text-center">
-						<span class="fs-4 text-danger">????</span><br />EPA
+						<span class="fs-4 text-danger">{data.nextmatch.alliances.red.team_keys[0]}</span><br
+						/>EPA
 					</div>
-					<div class="col text-center"><span class="fs-4 text-danger">????</span><br />EPA</div>
-					<div class="col text-center"><span class="fs-4 text-danger">????</span><br />EPA</div>
+					<div class="col text-center">
+						<span class="fs-4 text-danger">{data.nextmatch.alliances.red.team_keys[1]}</span><br
+						/>EPA
+					</div>
+					<div class="col text-center">
+						<span class="fs-4 text-danger">{data.nextmatch.alliances.red.team_keys[2]}</span><br
+						/>EPA
+					</div>
 				</div>
 				<div class="row mb-3">
-					<div class="col text-center">
+					<!-- <div class="col text-center">
 						<span class="fs-2 text-danger fw-bold">Score</span> :
 						<span class="fs-3 text-primary">Score</span>
 						<br />Projected Winner:
-						<span class="text-danger">Color</span>
-					</div>
+						<span class={data.nextmatch.pred.winner == 'red' ? 'text-danger' : 'text-primary'}
+							>{titleCase(data.nextmatch.pred.winner)}</span
+						>
+					</div> -->
 					<div class="col text-center">
-						<span class="fs-2 text-danger">64%</span><br />Win Probability
+						<span class="fs-2"
+							>{Math.round(
+								data.nextmatch.alliances.red.team_keys.includes(data.team)
+									? data.nextmatch.pred.red_win_prob * 100
+									: 100 - data.nextmatch.pred.red_win_prob * 100
+							)}%</span
+						><br />Win Probability
 					</div>
 				</div>
 				<div class="row">
-					<div class="col text-center"><span class="fs-4 text-primary">????</span><br />55</div>
-					<div class="col text-center"><span class="fs-4 text-primary">????</span><br />55</div>
-					<div class="col text-center"><span class="fs-4 text-primary">????</span><br />55</div>
+					<div class="col text-center">
+						<span class="fs-4 text-primary">{data.nextmatch.alliances.blue.team_keys[0]}</span><br
+						/>EPA
+					</div>
+					<div class="col text-center">
+						<span class="fs-4 text-primary">{data.nextmatch.alliances.blue.team_keys[1]}</span><br
+						/>EPA
+					</div>
+					<div class="col text-center">
+						<span class="fs-4 text-primary">{data.nextmatch.alliances.blue.team_keys[2]}</span><br
+						/>EPA
+					</div>
 				</div>
 			</div>
 		</div>
 		<div class="col">
 			<div class="bin">
 				<h4>Upcomming Matches</h4>
-				<TableForObjectArray data={upcommingMatches} columns={matchesColumns} />
+				<TableForObjectArray data={data.upcommingMatches} columns={matchesColumns} />
 			</div>
 		</div>
 	</div>
 
 	<!-- Our Stats -->
-	<div class="row">
+	<div class="row mt-3">
 		<div class="col-6">
 			<div class="text-center bin d-flex align-items-center justify-content-center gap-3">
 				<h4>Rank by EPA</h4>
-				<Badge value={'?'} prefix={'Event'} desc={'out of 3//'} />
+				<Badge
+					value={data.ourRanking.record.qual.rank}
+					prefix={'Event'}
+					desc={`out of ${data.ourRanking.record.qual.num_teams}`}
+				/>
 				<Badge
 					value={data.teamYear.epa.ranks.state.rank}
 					prefix={'Massachusetts'}
@@ -115,7 +114,7 @@
 				/>
 				<Badge
 					value={data.teamYear.epa.ranks.district.rank}
-					prefix={'NE'}
+					prefix={'New England'}
 					desc={`out of ${data.teamYear.epa.ranks.district.team_count}`}
 				/>
 				<Badge
@@ -143,10 +142,22 @@
 		<div class="col-4">
 			<div class="text-center bin d-flex align-items-center justify-content-center gap-3">
 				<h4>EPA Stats</h4>
-				<Badge value={'?'} prefix={'Auto EPA'} />
-				<Badge value={'?'} prefix={'Teleop EPA'} color={'rgb(255, 127, 14)'} />
-				<Badge value={'?'} prefix={'Endgame EPA'} color={'rgb(44, 160, 44)'} />
-				<Badge value={'?'} prefix={'Total EPA'} color={'rgb(214, 39, 40)'} />
+				<Badge value={Math.round(data.ourRanking.epa.breakdown.auto_points)} prefix={'Auto EPA'} />
+				<Badge
+					value={Math.round(data.ourRanking.epa.breakdown.teleop_points)}
+					prefix={'Teleop EPA'}
+					color={'rgb(255, 127, 14)'}
+				/>
+				<Badge
+					value={Math.round(data.ourRanking.epa.breakdown.endgame_points)}
+					prefix={'Endgame EPA'}
+					color={'rgb(44, 160, 44)'}
+				/>
+				<Badge
+					value={Math.round(data.ourRanking.epa.breakdown.total_points)}
+					prefix={'Total EPA'}
+					color={'rgb(214, 39, 40)'}
+				/>
 			</div>
 		</div>
 	</div>
